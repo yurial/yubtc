1e191d5aec0a9d502865f80b8ced04053378922f
Yuri Dyachenko <pub.yurial@gmail.com>
2019-05-03 11:51:53 +0300

    remove cycle from seed2privkey.py

--- seed2privkey.py
+++ seed2privkey.py
@@ -1,14 +1,11 @@
 #!/usr/bin/env python3
 
-while True:
-    from getpass import getpass
-    from misc import yesno
-    from crypto import seed2privkey, privkey2addr, privkey2privwif
-    seed = getpass('seed: ')
-    privkey = seed2privkey(seed)
-    print('uncompressed privkey: {}'.format(privkey2privwif(privkey, compressed=False)))
-    print('uncompressed address: {}'.format(privkey2addr(privkey, compressed=False)))
-    print('compressed privkey: {}'.format(privkey2privwif(privkey, compressed=True)))
-    print('compressed address: {}'.format(privkey2addr(privkey, compressed=True)))
-    if yesno('exit? '):
-        break
+from getpass import getpass
+from misc import yesno
+from crypto import seed2privkey, privkey2addr, privkey2privwif
+seed = getpass('seed: ')
+privkey = seed2privkey(seed)
+print('uncompressed privkey: {}'.format(privkey2privwif(privkey, compressed=False)))
+print('uncompressed address: {}'.format(privkey2addr(privkey, compressed=False)))
+print('compressed privkey: {}'.format(privkey2privwif(privkey, compressed=True)))
+print('compressed address: {}'.format(privkey2addr(privkey, compressed=True)))
